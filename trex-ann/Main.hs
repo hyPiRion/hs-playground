ffea9187ae81fbff70aa22709515d5f658e8ae7c
Jean Niklas L'orange <jeannikl@hypirion.com>
2014-10-09 17:08:23 +0200

    Example of composition.

--- trex-ann/Main.hs
+++ trex-ann/Main.hs
@@ -3,14 +3,42 @@
 import Data.OpenRecords
 import Example.Ast
 import Example.Pass.Typecheck
+import Example.Pass.Depth
 
-e :: RawExpr
-e = iIf (iNot (iCmp (iLInt 10) (iLInt 20)))
+exampleExpr :: RawExpr
+exampleExpr = iIf (iNot (iCmp (iLInt 10) (iLInt 20)))
     (iAdd (iLInt 3) (iLInt 6))
     (iLInt 0)
 
-eTyped :: AnnExpr ("type'" ::= Type :| Empty)
-eTyped = inferType e
+-- Better leave these type signatures out when possible. They aren't immediately
+-- obvious, and depends on ordering when they're as free as they are here.
+
+-- Another option I will check out is avoiding the :\ constraints, if that helps.
+nanopass :: ((("depth" ::= Int :| "type'" ::= Type :| r0) :! "depth") ~ Int,
+             (("type'" ::= Type :| r0) :! "type'") ~ Type,
+             r0 :\ "type'",
+             ("type'" ::= Type :| r0) :\ "depth")
+             => AnnExpr r0 -> AnnExpr ("depth" ::= Int :| "type'" ::= Type :| r0)
+nanopass = annDepth . inferType
+nanopass2 = inferType . annDepth
+
+-- Although this is illegal according to our constraints, the type checker
+-- cannot confirm this because, well, I don't know. But the last constraint is
+-- clearly illegal (The others are okay)
+broken :: ((("depth" ::= Int :| "depth" ::= Int :| r0) :! "depth") ~ Int,
+           (("depth" ::= Int :| r0) :! "depth") ~ Int,
+           r0 :\ "depth",
+           ("depth" ::= Int :| r0) :\ "depth")
+          => AnnExpr r0 -> AnnExpr ("depth" ::= Int :| "depth" ::= Int :| r0)
+broken e = annDepth (annDepth e)
+
+-- This fails at compile time:
+-- tryBroken = broken exampleExpr
+
+annotated :: AnnExpr ("depth" ::= Int :| "type'" ::= Type :| Empty)
+annotated = nanopass exampleExpr -- this signature matches nanopass2 as well
 
 main :: IO ()
-main = print e >> print eTyped
+main = do print exampleExpr
+          print $ nanopass exampleExpr
+          print $ nanopass2 exampleExpr == nanopass exampleExpr
