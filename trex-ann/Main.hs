c9e33b7be6e166e133500e34e6ca09463775c634
Jean Niklas L'orange <jeannikl@hypirion.com>
2014-10-09 15:45:03 +0200

    Example (with only 1 pass for now)

--- trex-ann/Main.hs
+++ trex-ann/Main.hs
@@ -1,2 +1,16 @@
+{-# LANGUAGE TypeOperators, TypeFamilies, DataKinds #-}
+
+import Data.OpenRecords
+import Example.Ast
+import Example.Pass.Typecheck
+
+e :: RawExpr
+e = iIf (iNot (iCmp (iLInt 10) (iLInt 20)))
+    (iAdd (iLInt 3) (iLInt 6))
+    (iLInt 0)
+
+eTyped :: AnnExpr ("type'" ::= Type :| Empty)
+eTyped = inferType e
+
 main :: IO ()
-main = putStrLn "Hello world!"
+main = print e >> print eTyped
